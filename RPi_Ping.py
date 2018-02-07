fc7c8fc48bd08654ee18077cb841b8005046892e
CarlosAndresB <carlosandresb@tuum.io>
2018-02-07 11:32:31 +0100

    Update RPi_Ping.py

--- RPi_Ping.py
+++ RPi_Ping.py
@@ -6,7 +6,7 @@ import sys
 import socket
 #import getopt
 from decimal import Decimal
-from platform import system as sys_name
+#from platform import system as sys_name
 
 resultados = {} #Diccionario donde se guardan los resultados
 puertos = {}
@@ -34,44 +34,48 @@ def scan(host,port):
 def file_n():
 	fm = "pinglog" + str(date.today()) + ".txt"
 	return fm
-			     
-#def usage():
+
+"""
+def usage():
        
-       #print("Uso: Ping")
-       #print("")
-       #print("-i Inicio")
-       #print("-f Fin")
-       #print("-d Direccion")
-       #print("-h info de ayuda")
+       print("Uso: Ping")
+       print("")
+       print("-i Inicio")
+       print("-f Fin")
+       print("-d Direccion")
+       print("-h info de ayuda")
 
-#try:
-       #opts, args = getopt.getopt(sys.argv[1:], "i:f:d:h")
+try:
+       opts, args = getopt.getopt(sys.argv[1:], "i:f:d:h")
 
-#except getopt.GetoptError as err:
-       #print("Error: "+str(err)+", Intente -h para ayuda\n\n")
-       #usage()
-       #sys.exit(2)
+except getopt.GetoptError as err:
+       print("Error: "+str(err)+", Intente -h para ayuda\n\n")
+       usage()
+       sys.exit(2)
 
-#for o, a in opts:
-       #if o in ("-h"):
-              #usage()
-              #sys.exit()
-       #if o in ("-i"):
-              #y = Decimal(a)
-       #if o in ("-f"):
-              #x = Decimal(a)
-       #if o in ("-d"):
-              #d = a
+for o, a in opts:
+       if o in ("-h"):
+              usage()
+              sys.exit()
+       if o in ("-i"):
+              y = Decimal(a)
+       if o in ("-f"):
+              x = Decimal(a)
+       if o in ("-d"):
+              d = a
+"""
+
 y = Decimal(input("Direccion Inicio: "))
 x = Decimal(input("Direccion Fin: "))
 p = Decimal(input("Puerto: "))
 a = "192.168.1"
-parameters = "-n 1 " if sys_name().lower()=="windows" else "-c 1 "
+
+#parameters = "-n 1 " if sys_name().lower()=="windows" else "-c 1 "
 while y <= x:
        GPIO.output(led,1)
        direc = a + "." + str(y)
-       #response = os.system("ping -c 1 " + direc)
-       response = os.system("ping " + parameters + direc)
+       response = os.system("ping -c 1 " + direc)
+       #response = os.system("ping " + parameters + direc)
        if response == 0:
               pingstatus = "Ususario Activo"
               resultados[y] = pingstatus
